73ea360e1ab4b8cc6963f460729f74a3af4244c8
Sergii Getman <sergeygetman2020@gmail.com>
2023-08-21 21:08:09 +0300

    fix lint

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,18 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from '../styles/globalStyle.module.css';
 import Button from './button';
 import { useNavigate } from 'react-router-dom';
 
 const Container = () => {
-  const [openHandler, setOpenHandler] = useState(false);
-  const [selectedIdx, setSelectedIdx] = useState<number | null>(null);
-
   const navigate = useNavigate();
-
-  const handleClickFC = (idx: number) => {
-    setOpenHandler(!openHandler);
-    setSelectedIdx(idx);
-  };
 
   const handleClickNavigate = () => {
     navigate('/coursea');
